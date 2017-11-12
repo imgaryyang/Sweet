f7d0aa3be5d6187930a3f65b8a74bdd686bbba4d
chinn96 <452896150@qq.com>
2017-11-12 19:48:03 +0800

    change version

--- app/src/main/java/com/lucky/sweet/activity/MainActivity.java
+++ app/src/main/java/com/lucky/sweet/activity/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //测试
+        //测试垃圾玩意！！！！
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
