c12ddfe2aa47a2aca018ac5b80e74eb915023fea
chinn96 <452896150@qq.com>
2017-11-12 19:11:38 +0800

    First Test

--- app/src/main/java/com/lucky/sweet/activity/MainActivity.java
+++ app/src/main/java/com/lucky/sweet/activity/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //测试
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
