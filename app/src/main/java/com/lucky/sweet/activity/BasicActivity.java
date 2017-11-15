31e5b8c0bc701972e94236062dbcd92f207bd8de
chinn <chinn96@163.com>
2017-11-15 15:20:28 +0800

    create RegisterLayout and LoginLayout

---
+++ app/src/main/java/com/lucky/sweet/activity/BasicActivity.java
@@ -0,0 +1,13 @@
+package com.lucky.sweet.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BasicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
