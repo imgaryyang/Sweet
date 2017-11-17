3d91af65adad59e06efb28925553f87ddbfe5539
Chinn <chinn96@163.com>
2017-11-17 17:23:48 +0800

    test

--- app/src/main/java/com/lucky/sweet/activity/BasicActivity.java
+++ app/src/main/java/com/lucky/sweet/activity/BasicActivity.java
@@ -8,6 +8,7 @@ public class BasicActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //test
     }
 
 }
