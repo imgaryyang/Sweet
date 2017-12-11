259f18ecd103180b2570cb2a5a926a5d1bc46509
Chinn <chinn96@163.com>
2017-12-11 22:50:32 +0800

    updata

---
+++ app/src/main/java/com/lucky/sweet/views/ScrollingImageViewBitmapLoader.java
@@ -0,0 +1,11 @@
+package com.lucky.sweet.views;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+/**
+ * Created by thijs on 22-03-16.
+ */
+public interface ScrollingImageViewBitmapLoader {
+    Bitmap loadBitmap(Context context, int resourceId);
+}
