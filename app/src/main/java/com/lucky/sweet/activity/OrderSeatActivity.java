dbcb768821d471d691a00351b446b5c059bd7231
Qiuyue <QY983983@163.com>
2017-12-19 23:34:46 +0800

    添加预订座位页面

---
+++ app/src/main/java/com/lucky/sweet/activity/OrderSeatActivity.java
@@ -0,0 +1,22 @@
+package com.lucky.sweet.activity;
+
+import android.os.Bundle;
+
+import com.lucky.sweet.R;
+
+/**
+ * Created by Qiuyue on 2017/12/19.
+ */
+
+// ╭︿︿︿╮  ╭︿︿︿╮  ╭︿︿︿╮
+// {/ o o /} {/ . . /} {/ ︿︿ /}
+// ( (oo) )  ( (oo) )  ( (oo) )
+//   ︶︶︶     ︶︶︶     ︶︶︶
+
+public class OrderSeatActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orderseat);
+    }
+}
