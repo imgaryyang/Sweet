2d2026bfd81890dda59d1f5fe1c2f41550683a2c
chinn <33593253+Chinnko@users.noreply.github.com>
2018-01-18 11:24:41 +0800

    添加检索栏 快捷点击

--- app/src/main/java/com/lucky/sweet/activity/BaseActivity.java
+++ app/src/main/java/com/lucky/sweet/activity/BaseActivity.java
@@ -64,7 +64,6 @@ public abstract class BaseActivity extends AppCompatActivity {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder
                 iBinder) {
-
             CommunicationService.MyBinder myBinder = (CommunicationService.MyBinder) iBinder;
             onServiceBind(myBinder);
 
