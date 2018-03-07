a5b8f724e7d6c44471a27947cca979334b5bb6c9
Qiuyue <QY983983@163.com>
2018-03-07 15:28:19 +0800

    增加等待页面

--- app/src/main/java/com/lucky/sweet/activity/FloatPageActivity.java
+++ app/src/main/java/com/lucky/sweet/activity/FloatPageActivity.java
@@ -1,6 +1,9 @@
 package com.lucky.sweet.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.lucky.sweet.R;
 import com.lucky.sweet.service.CommunicationService;
@@ -16,6 +19,11 @@ import com.lucky.sweet.widgets.ToolBar;
 //   ︶︶︶     ︶︶︶     ︶︶︶
 
 public class FloatPageActivity extends BaseActivity {
+
+    private View view_null1;
+    private View view_null2;
+    private Button btn_wait;
+
     @Override
     void onServiceBind(CommunicationService.MyBinder myBinder) {
 
@@ -25,7 +33,42 @@ public class FloatPageActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floatpage);
-        ToolBar toolBar=new ToolBar(this);
-        toolBar.setImmersionBar();
+        initViews();
+        initEvent();
     }
+
+    private void initEvent() {
+        view_null1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                goFadeAnim();
+            }
+        });
+        view_null2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                goFadeAnim();
+            }
+        });
+        btn_wait.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FloatPageActivity.this,
+                        WaitOrderActivity.class);
+                startActivity(intent);
+                goNextAnim();
+            }
+        });
+    }
+
+    private void initViews() {
+        ToolBar toolBar = new ToolBar(this);
+        toolBar.setImmersionBar();
+        view_null1 = (View) findViewById(R.id.view_null1);
+        view_null2 = (View) findViewById(R.id.view_null2);
+        btn_wait = (Button) findViewById(R.id.btn_wait);
+    }
+
 }
